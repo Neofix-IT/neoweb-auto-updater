d1ea0e36acf0e2ad446fcbbbefa906a97365b537
Martin Heini <maheini35@me.com>
2024-06-12 23:49:38 +0200

    changed logger file location

--- logger.php
+++ logger.php
@@ -12,16 +12,23 @@
 */ 
 
 function neoweb_log($message){
-
     if (defined('NEOWEB_UPDATER_PATH')) {
-        $tz = new DateTimeZone(NEOWEB_UPDATE_TZ);
-        $now = new DateTimeImmutable('now', $tz);
-        $log_file = NEOWEB_UPDATER_PATH . "/log.txt";
+        $now = current_datetime();
+        $log_file = WP_CONTENT_DIR . "/neoweb_autoupdater.log";
         
         file_put_contents($log_file, $now->format('Y-m-d H:i:s') . ": " . print_r( $message, true ) ."\n", FILE_APPEND);
     } else {
         error_log('Neoweb auto-updater NEOWEB_UPDATER_PATH not defined - logging impossible');
     }
     return;
+}
+
+register_deactivation_hook( NEOWEB_UPDATER_PLUGINFILE_PATH ,  'neoweb_updater_delete_logfile' );
+
+function neoweb_updater_delete_logfile(){
+    $log_file = WP_CONTENT_DIR . "/neoweb_autoupdater.log";
+    if ( file_exists($log_file)){
+        unlink($log_file);
+    }
 }
 ?>
