7d230a3b2fdd0f7e7725f8e1005390338648e9ad
Martin Heini <maheini35@me.com>
2024-10-02 13:33:01 +0200

    switch to logger class

--- logger.php
+++ logger.php
@@ -11,24 +11,31 @@
  * 
 */ 
 
-function neoweb_log($message){
-    if (defined('NEOWEB_UPDATER_PATH')) {
-        $now = current_datetime();
+class NeowebLoger{
+    function __construct(){
+        register_deactivation_hook( NEOWEB_UPDATER_PLUGINFILE_PATH ,  array($this, "deactivate_actions") );
+    }
+
+    public function log($message){
+        if (defined('NEOWEB_UPDATER_PATH') && NEOWEB_UPDATER_PATH) {
+            $now = current_datetime();
+            $log_file = WP_CONTENT_DIR . "/neoweb_autoupdater.log";
+            
+            file_put_contents($log_file, $now->format('Y-m-d H:i:s') . ": " . print_r( $message, true ) ."\n", FILE_APPEND);
+
+        } else {
+            error_log('Neoweb auto-updater NEOWEB_UPDATER_PATH not defined - logging impossible');
+        }
+    }
+
+    private function deactivate_actions(){
+        // delete logfile
         $log_file = WP_CONTENT_DIR . "/neoweb_autoupdater.log";
-        
-        file_put_contents($log_file, $now->format('Y-m-d H:i:s') . ": " . print_r( $message, true ) ."\n", FILE_APPEND);
-    } else {
-        error_log('Neoweb auto-updater NEOWEB_UPDATER_PATH not defined - logging impossible');
-    }
-    return;
-}
-
-register_deactivation_hook( NEOWEB_UPDATER_PLUGINFILE_PATH ,  'neoweb_updater_delete_logfile' );
-
-function neoweb_updater_delete_logfile(){
-    $log_file = WP_CONTENT_DIR . "/neoweb_autoupdater.log";
-    if ( file_exists($log_file)){
-        unlink($log_file);
+        if ( file_exists($log_file)){
+            unlink($log_file);
+        }
     }
 }
+
+$logger = new NeowebLoger();
 ?>
