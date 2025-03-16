b1ba0f9b125b8702d71e2d8566a6ae56f5e31fdd
Martin Heini <maheini35@me.com>
2025-03-17 00:57:35 +0100

    feat: apply modular version of this plugin

--- neoweb-auto-updater.php
+++ neoweb-auto-updater.php
@@ -21,10 +21,36 @@ if (! defined('ABSPATH')) {
 define('NEOWEB_UPDATER_PATH', plugin_dir_path(__FILE__));
 define('NEOWEB_UPDATER_PLUGINFILE_PATH', __FILE__);
 
-// Enable Test-update admin panel menu page
-define('NEOWEB_UPDATER_ADMINPANEL_VISIBLE', true);
+// Log length
 define('NEOWEB_LOG_LENGTH', 150);
 
-require_once 'modules/logger.php';
-require_once 'scheduled_update.php';
-require_once 'admin-panel.php';
+require_once NEOWEB_UPDATER_PATH . 'modules/logger.php';
+require_once NEOWEB_UPDATER_PATH . 'scheduled_update.php';
+require_once NEOWEB_UPDATER_PATH . 'modules/updater.php';
+require_once NEOWEB_UPDATER_PATH . 'modules/github-updater.php';
+require_once NEOWEB_UPDATER_PATH . 'admin-panel.php';
+
+
+class NeowebAutoUpdater
+{
+	private $logger;
+	private $updater;
+	private $scheduler;
+	private $admin_panel;
+	private $github_updater;
+
+	function __construct()
+	{
+		$this->logger = new NeowebLogger();
+		$this->updater = new NeowebUpdater($this->logger);
+		$this->scheduler = new NeowebUpdateScheduler($this->updater, $this->logger);
+		$this->admin_panel = new NeowebAdminPanel($this->updater, $this->logger);
+
+		// require_once 'modules/github-updater.php';
+		$this->github_updater = new GitHubAutoUpdater(
+			'neoweb-auto-updater/neoweb-auto-updater.php',
+			'Neofix-IT/neoweb-auto-updater'
+		);
+	}
+}
+new NeowebAutoUpdater();
