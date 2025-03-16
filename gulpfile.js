8edc5cd6764fa491e1d425b4e1cd463555f8ab96
Martin Heini <maheini35@me.com>
2025-03-16 15:57:54 +0100

    build: auto-bundle plugin with outer plugin folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,57 @@
 import gulp from "gulp";
 import zip from "gulp-zip";
+import { deleteAsync } from "del"; // To handle cleaning up old files
+import fs from "fs";
 
 // Plugin details
-const pluginName = "neoweb-auto-updater"; // Replace with your plugin folder name
-const outputFileName = `${pluginName}.zip`;
+const outerFolderName = "neoweb-auto-updater"; // Name of the folder in /build
+const outputFileName = `${outerFolderName}.zip`; // Name of the ZIP file
 
 // Paths
-const pluginDir = "./"; // The root directory of your plugin
-const destination = "./dist"; // Output folder for the ZIP file
+const pluginDir = "./"; // Root directory containing plugin files
+const buildFolder = "./build"; // Folder where 'neoweb-auto-updater' will be created
+const tempFolder = `${buildFolder}/${outerFolderName}`; // Path to the new folder
+const destination = "./dist"; // Output directory for the ZIP file
 
-// Task to create ZIP
-gulp.task("zip", function () {
-  return gulp
-    .src([
-      `${pluginDir}/**/*`, // Include all files and folders
-      `!${pluginDir}/dist/**`, // Exclude 'dist' folder itself
-      `!${pluginDir}/node_modules/**`, // Exclude node_modules folder
-      `!${pluginDir}/.git/**`, // Exclude .git folder
-      `!${pluginDir}/gulpfile.js`, // Exclude the gulpfile itself
-      `!${pluginDir}/README.md`, // Exclude the gulpfile itself
-      `!${pluginDir}/package*`, // Exclude package.json and package-lock.json
-    ])
-    .pipe(zip(outputFileName))
-    .pipe(gulp.dest(destination));
+// Task: Clean /build and /dist folders
+gulp.task("clean", function () {
+  return deleteAsync([buildFolder, destination]);
 });
 
-// Default task
-gulp.task("default", gulp.series("zip"));
+// Task: Create 'neoweb-auto-updater' folder and copy all files
+gulp.task("prepare", function () {
+  // Ensure the destination folders exist
+  if (!fs.existsSync(buildFolder)) {
+    fs.mkdirSync(buildFolder);
+  }
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination);
+  }
+
+  return gulp
+    .src([
+      `${pluginDir}/**/*`, // Include all plugin files
+      `!${pluginDir}/build/**`, // Exclude build folder itself
+      `!${pluginDir}/dist/**`, // Exclude dist folder
+      `!${pluginDir}/node_modules/**`, // Exclude node_modules folder
+      `!${pluginDir}/.git/**`, // Exclude .git folder
+      `!${pluginDir}/gulpfile.js`, // Exclude gulpfile
+      `!${pluginDir}/README.md`, // Exclude README file
+      `!${pluginDir}/package*`, // Exclude package.json and package-lock.json
+    ])
+    .pipe(gulp.dest(tempFolder)); // Copy files into /build/neoweb-auto-updater
+});
+
+// Task: Zip the 'neoweb-auto-updater' folder
+gulp.task("zip", function () {
+  return gulp
+    .src(`${tempFolder}/**/*`, { base: buildFolder }) // Include the outer folder
+    .pipe(zip(outputFileName)) // Create the ZIP file
+    .pipe(gulp.dest(destination)); // Save the ZIP in /dist
+});
+
+// Default task: Clean -> Prepare -> Zip
+gulp.task("build", gulp.series("clean", "prepare", "zip"));
+
+// Default task: Clean -> Prepare -> Zip
+gulp.task("default", gulp.series("clean", "prepare", "zip"));
